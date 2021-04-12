b69337b06f68353b98a8fa3e945fe7f1a23d4594
Phil Winder <phil@WinderResearch.com>
2021-04-12 21:44:00 +0100

    first working version

--- pachyderm.tf
+++ pachyderm.tf
@@ -8,12 +8,13 @@ terraform {
 }
 
 resource "helm_release" "pachyderm" {
-  name             = "pachyderm"
+  name = "pachyderm"
   repository       = "https://pachyderm.github.io/helmchart/"
-  chart            = "pachyderm"
-  version          = ""
+  chart      = "pachyderm"
+  version          = "0.4.1"
   namespace        = var.namespace
-  wait             = false
+  wait             = true
+  lint             = true
   values           = var.values
   create_namespace = true
 }
--- variables.tf
+++ variables.tf
@@ -8,8 +8,13 @@ variable "values" {
   description = "(Optional) List of values in raw yaml to pass to helm. See https://github.com/pachyderm/helmchart/blob/master/pachyderm/values.yaml."
   type        = list(string)
   default = [<<EOT
+tls:
+  certName: null # Disable TLS
+  create: null # Disable TLS
 pachd:
   logLevel: debug
+  storage:
+    backend: LOCAL
 EOT 
   ]
 }

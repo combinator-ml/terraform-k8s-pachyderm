d41e9fcd9049d4a8cea646b5ad2ad3a6b09ddd80
Phil Winder <phil@WinderResearch.com>
2021-05-22 10:20:38 +0100

    tests: add pachyderm dash to testfaster

--- examples/testfaster/main.tf
+++ examples/testfaster/main.tf
@@ -1,3 +1,28 @@
+variable "namespace" {
+  description = "(Optional) The namespace to install into. Defaults to feast."
+  type        = string
+  default     = "pachyderm"
+}
+
 module "pachyderm" {
-  source = "combinator-ml/pachyderm/k8s"
+  source    = "combinator-ml/pachyderm/k8s"
+  namespace = var.namespace
+}
+
+resource "kubernetes_service" "pachyderm-dash" {
+  metadata {
+    name      = "testfaster-pachyderm-dash"
+    namespace = var.namespace
+  }
+  spec {
+    selector = {
+      app = "dash"
+    }
+    port {
+      port        = 8080
+      target_port = 8080
+      node_port   = 30600
+    }
+    type = "NodePort"
+  }
 }

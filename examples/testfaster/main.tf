72c7474aa596d5f5c57656f7fdb65f402b61ed85
Phil Winder <phil@winderresearch.com>
2021-05-24 16:47:05 +0100

    feat: pachyderm demo in jupyter notebook
    * test: add jupyter demo
    * test: update demo and testfaster links
    * test: final improvements to Pachyderm Demo
    * fix: fix linting
    * test: cache buster 4000

--- examples/testfaster/main.tf
+++ examples/testfaster/main.tf
@@ -1,10 +1,16 @@
 variable "namespace" {
-  description = "(Optional) The namespace to install into. Defaults to feast."
+  description = "The namespace to install into."
+  default     = "default"
   type        = string
-  default     = "pachyderm"
+}
+
+locals {
+  prefix           = "testfaster"
+  create_namespace = var.namespace == "default" ? 0 : 1 # Create if not default
 }
 
 resource "kubernetes_namespace" "namespace" {
+  count = local.create_namespace
   metadata {
     name = var.namespace
   }
@@ -18,9 +24,39 @@ module "pachyderm" {
   ]
 }
 
+module "jupyter" {
+  source    = "combinator-ml/jupyter/k8s"
+  namespace = var.namespace
+  depends_on = [
+    kubernetes_namespace.namespace
+  ]
+}
+
+resource "kubernetes_service" "jupyter" {
+  metadata {
+    name      = "${local.prefix}-jupyter"
+    namespace = var.namespace
+  }
+  spec {
+    selector = {
+      app = "combinator-jupyter"
+    }
+    port {
+      name        = "http"
+      port        = 8888
+      target_port = 8888
+      node_port   = 30600
+    }
+    type = "NodePort"
+  }
+  depends_on = [
+    module.jupyter
+  ]
+}
+
 resource "kubernetes_service" "pachyderm-dash" {
   metadata {
-    name      = "testfaster-pachyderm-dash"
+    name      = "${local.prefix}-pachyderm-dash"
     namespace = var.namespace
   }
   spec {
@@ -43,5 +79,16 @@ resource "kubernetes_service" "pachyderm-dash" {
   }
   depends_on = [
     module.pachyderm
+  ]
+}
+
+# Wait for the pod to start then copy the notebook into the working directory of the pod
+# Any changes will only exist inside the pod. Download the notebook if you want a copy.
+resource "null_resource" "copy_notebook" {
+  provisioner "local-exec" {
+    command = "kubectl -n ${var.namespace} rollout status deployment/combinator-jupyter-deployment && kubectl -n ${var.namespace} cp demo.ipynb $(kubectl -n ${var.namespace} get po -l app=combinator-jupyter -o custom-columns=POD:.metadata.name --no-headers):/home/jovyan"
+  }
+  depends_on = [
+    module.jupyter
   ]
 }
